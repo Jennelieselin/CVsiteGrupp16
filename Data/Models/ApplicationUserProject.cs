e7f538fc04a5d4991a0b52420893b760662dbf83
Jennelieselin <76255570+Jennelieselin@users.noreply.github.com>
2022-01-05 10:42:44 +0100

    lite fel

--- Data/Models/ApplicationUserProject.cs
+++ Data/Models/ApplicationUserProject.cs
@@ -1,27 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Data.Models
 {
-    public  class ApplicationUserProject
+    public class ApplicationUserProject
     {
-       [Key]
+        [Key]
         [Column(Order = 1)]
         [Required]
-       
         public int ProjectId { get; set; }
         [Key]
         [Column(Order = 2)]
         [Required]
         public string UserId { get; set; }
         [Required]
-        public string Username{ get; set; }
-
-
+        [Display(Name = "Användare")]
+        public string UserName { get; set; }
     }
 }
